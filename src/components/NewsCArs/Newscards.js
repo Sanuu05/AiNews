db50ac01d33cca86955f1986f8688333472b0c36
Shantanu Saha <sansaha0505@gmail.com>
2022-05-04 01:20:02 +0530

    first commit

--- src/components/NewsCArs/Newscards.js
+++ src/components/NewsCArs/Newscards.js
@@ -6,9 +6,9 @@ function Newscards({ data,activeart }) {
     console.log('data2', data)
     // const classes = useStyles()
     const infoCards = [
-        { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+        { color: '#00838f', title: 'Latest News', text: 'What are the latest news' },
         { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
-        { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
+        { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'News about PlayStation 5' },
         { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
     ];
     if (!data.length) {
--- src/components/Newscard/Newscard.js
+++ src/components/Newscard/Newscard.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import {Card,CardActions,CardActionArea,CardClasses,CardMedia,Button,Typography, CardContent} from '@mui/material'
+import { useEffect ,createRef} from 'react'
+import { useState } from 'react'
 function Newscard({data:{description,publishedAt,source,title,url,urlToImage},i,activeart}) {
   console.log('cv',i)
   console.log('cvv',activeart)
+  const [elRefs, setelRefs] = useState([])
+  const rollRef = ((ref)=>window.scroll(0,ref.current.offsetTop - 50))
+  useEffect(()=>{
+    setelRefs((ref)=>Array(20).fill().map((_,j)=>ref[j] || createRef()))
+
+  })
+  useEffect(()=>{
+    if(i===activeart && elRefs[activeart]){
+      rollRef(elRefs[activeart])
+    }
+
+  },[i,activeart,elRefs])
   return (
-    <Card style={{display:'flex',flexDirection:'column',justifyContent:'space-between'}}  className={activeart===i?"cardbtm":null} >
+    <Card ref={elRefs[i]} style={{display:'flex',flexDirection:'column',justifyContent:'space-between'}}  className={activeart===i?"cardbtm":null} >
       <CardActionArea href={url} target='_blank'>
         <CardMedia image={urlToImage} style={{height:250}}/>
         <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center',margin:'7px 10px '}}>
